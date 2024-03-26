5fea67445e2b27e51f85390b2616408cfd02d27f
Tushar Goyal <tushar462024@gmail.com>
2024-03-27 02:16:05 +0530

    Generate only static files related changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/tw-gridview",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
